9ee949fa67f269a09abc26b2d162e96fa019e93f
gianniskokTr <y.kokkoros@trebbble.co>
2024-02-11 14:03:08 +0200

    .

--- app/api/images/inactive/route.tsx
+++ app/api/images/inactive/route.tsx
@@ -1,10 +1,10 @@
 import { ImageResponse } from 'next/og';
 import { Card } from '../../../components/Card';
-import { CARD_DIMENSIONS } from '../../../config';
+import {CARD_DIMENSIONS, NEXT_PUBLIC_URL} from '../../../config';
 
 export async function GET() {
   return new ImageResponse(
-    <Card message="You can mint with Warps below." image={'${NEXT_PUBLIC_URL}/horse.png'}/>,
+    <Card message="You can mint with Warps below." image={`${NEXT_PUBLIC_URL}/horse.png`}/>,
     CARD_DIMENSIONS,
   );
 }
