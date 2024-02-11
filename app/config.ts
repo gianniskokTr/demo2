29ee2ab1fc75ab6cc1ccdb380757c86dce58630f
gianniskokTr <y.kokkoros@trebbble.co>
2024-02-11 02:57:21 +0200

    .

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-export const NEXT_PUBLIC_URL = 'https://base-mint-with-warps-psi.vercel.app';
-export const ALLOWED_ORIGIN = 'base-mint-with-warps-psi.vercel.app';
+export const NEXT_PUBLIC_URL = 'https:/demo2-phi.vercel.app';
+export const ALLOWED_ORIGIN = 'demo2-phi.vercel.app';
 export const ZORA_COLLECTION_ADDRESS = '0xf5a3b6dee033ae5025e4332695931cadeb7f4d2b';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
